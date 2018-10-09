8fe94e7052206120dfbd1a331f18361a2d366213
haonanDu999 <503970903@qq.com>
2018-10-09 11:14:12 +0800

    将.idea文件夹忽略

--- user/models.py
+++ user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=12)
+    user_sex = models.BooleanField(default=True)
+    user_age = models.IntegerField(default=20)
+    user_icon = models.ImageField()
